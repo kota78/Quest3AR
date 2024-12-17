2ef30e3fe2297e5849e82374f11c95e94f55283e
kota78 <kota1725@gmail.com>
2024-12-17 19:32:07 +0900

    1217

--- marker_udp_2f.py
+++ marker_udp_2f.py
@@ -18,27 +18,31 @@ UDP_PORT = 50000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
 def send_data(data):
-    sock.sendto(data.encode('utf-8'), (UDP_IP, UDP_PORT))
+    sock.sendto(data.encode("utf-8"), (UDP_IP, UDP_PORT))
+
 
 def main():
     cap = cv2.VideoCapture(0)
     # マーカーサイズ
-    marker_length = 0.056 # [m]
+    marker_length = 0.056  # [m]
     # マーカーの辞書選択
     dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
-    
+
     # 自前Webカメラの内部パラメータ
     # camera_matrix = np.array([[1.33168655e+03,0.00000000e+00,9.54841857e+02],
     #                           [0.00000000e+00,1.33162266e+03,5.60401382e+02],
     #                           [0.00000000e+00,0.00000000e+00,1.00000000e+00]])
     # distortion_coeff = np.array([-0.35524981,0.01349514,-0.00471847,0.00897172,0.13142206])
-    
+
     # 小型カメラの内部パラメータ
-    camera_matrix = np.array([[464.76830292,0,512.35210634],
-                              [0,469.61837529,385.2203724],
-                              [0, 0, 1]])
-    distortion_coeff = np.array([-0.3136963, 0.10256045, -0.01269226, 0.00985827, -0.01445209])
+    camera_matrix = np.array(
+        [[464.76830292, 0, 512.35210634], [0, 469.61837529, 385.2203724], [0, 0, 1]]
+    )
+    distortion_coeff = np.array(
+        [-0.3136963, 0.10256045, -0.01269226, 0.00985827, -0.01445209]
+    )
 
     while True:
         ret, img = cap.read()
@@ -51,7 +55,9 @@ def main():
             # マーカーごとに処理
             for i, corner in enumerate(corners):
                 # rvec -> rotation vector, tvec -> translation vector
-                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corner, marker_length, camera_matrix, distortion_coeff)
+                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(
+                    corner, marker_length, camera_matrix, distortion_coeff
+                )
 
                 # < rodoriguesからeuluerへの変換 >
 
@@ -69,9 +75,15 @@ def main():
                 euler_angle = cv2.decomposeProjectionMatrix(proj_matrix)[6]  # [deg]
 
                 posX, posY, posZ = tvec[0], -tvec[1], tvec[2]  # Y座標を反転
-                rotX, rotY, rotZ = -euler_angle[0, 0], euler_angle[1, 0], euler_angle[2, 0]  # ロール軸を反転
+                rotX, rotY, rotZ = (
+                    -euler_angle[0, 0],
+                    euler_angle[1, 0],
+                    euler_angle[2, 0],
+                )  # ロール軸を反転
 
-                print(f"x: {posX:.2f}, y: {posY:.2f}, z: {posZ:.2f}, roll: {round(rotX)}, pitch: {round(rotY)}, yaw: {round(rotZ)}")
+                print(
+                    f"x: {posX:.2f}, y: {posY:.2f}, z: {posZ:.2f}, roll: {round(rotX)}, pitch: {round(rotY)}, yaw: {round(rotZ)}"
+                )
 
                 # データ送信
                 data = f"{posX:.2f},{posY:.2f},{posZ:.2f},{round(rotX)},{round(rotY)},{round(rotZ)}"
@@ -79,13 +91,16 @@ def main():
 
                 # 可視化
                 draw_pole_length = marker_length / 2  # 現実での長さ[m]
-                cv2.drawFrameAxes(img, camera_matrix, distortion_coeff, rvec, tvec, draw_pole_length)
+                cv2.drawFrameAxes(
+                    img, camera_matrix, distortion_coeff, rvec, tvec, draw_pole_length
+                )
 
-        cv2.imshow('drawDetectedMarkers', img)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        cv2.imshow("drawDetectedMarkers", img)
+        if cv2.waitKey(10) & 0xFF == ord("q"):
             break
 
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- markers_udp.py
+++ markers_udp.py
@@ -1,26 +1,37 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import numpy as np
 import cv2
 from cv2 import aruco
 import socket
 import time
 from scipy.spatial.transform import Rotation as R
-from config import UDP_IP, UDP_PORT, CAMERA_MATRIX, DISTORTION_COEFF, MARKER_LENGTH, CAM_HEIGHT, CAM_WIDTH
+import csv
+from config import (
+    UDP_IP,
+    UDP_PORT,
+    MARKER_LENGTH,
+    get_camera_config,
+)
+
+CAMERA_ID = 3
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
 def send_data(data):
-    sock.sendto(data.encode('utf-8'), (UDP_IP, UDP_PORT))
+    sock.sendto(data.encode("utf-8"), (UDP_IP, UDP_PORT))
+
 
 # 角度を 0°から360°の範囲に正規化
 def normalize_angle(angle):
     return angle % 360
 
+
 def main():
-    time.sleep(1)
     cap = cv2.VideoCapture(0)
     # 解像度を設定
+    CAM_WIDTH, CAM_HEIGHT, CAMERA_MATRIX, DISTORTION_COEFF = get_camera_config(
+        CAMERA_ID
+    )
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
     actual_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -28,27 +39,31 @@ def main():
     print(f"{actual_width}x{actual_height}")
 
     # マーカーの辞書選択
-    dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+    # 処理時間を保存するリスト
+    time_measurements = []
 
     while True:
-        data_list = []
+        t1 = time.time()
         ret, img = cap.read()
+        t2 = time.time()
         img = cv2.flip(img, -1)
+        data_list = []
 
         corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-        aruco.drawDetectedMarkers(img, corners, ids, (0, 255, 255))
-
         if len(corners) > 0:
             # マーカーごとに処理
             for i, corner in enumerate(corners):
-                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corner, MARKER_LENGTH, CAMERA_MATRIX, DISTORTION_COEFF)
-
+                rvec, tvec, _ = aruco.estimatePoseSingleMarkers(
+                    corner, MARKER_LENGTH, CAMERA_MATRIX, DISTORTION_COEFF
+                )
                 # 不要なaxisを除去
                 tvec = np.squeeze(tvec)
                 rvec = np.squeeze(rvec)
                 posX, posY, posZ = tvec[0], tvec[1], tvec[2]
-                rvec[0]=-rvec[0]
-                rvec[2]=-rvec[2]
+                rvec[0] = -rvec[0]
+                rvec[2] = -rvec[2]
                 # 回転ベクトル -> Rotationクラスのrotationオブジェクト
                 rotation = R.from_rotvec(rvec)
                 # Rotationクラスのrotationオブジェクト -> クォータニオン
@@ -57,21 +72,45 @@ def main():
                 data_list.append(data)
 
                 # 可視化
-                draw_pole_length = MARKER_LENGTH / 2
-                cv2.drawFrameAxes(img, CAMERA_MATRIX, DISTORTION_COEFF, rvec, tvec, draw_pole_length)
+                # aruco.drawDetectedMarkers(img, corners, ids, (0, 255, 255))
+                # draw_pole_length = MARKER_LENGTH /
+                # cv2.drawFrameAxes(
+                #     img, CAMERA_MATRIX, DISTORTION_COEFF, rvec, tvec, draw_pole_length
+                # )
 
-
-        if(data_list):
+        t3 = time.time()
+        if data_list:
             combined_data = ";".join(data_list)
             send_data(combined_data)
             print(combined_data)
 
-        cv2.imshow('drawDetectedMarkers', img)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        t4 = time.time()
+
+        # 処理時間をリストに保存
+        time_measurements.append([t2 - t1, t3 - t2, t4 - t3, t4 - t1])
+
+        # 可視化
+        # cv2.imshow("drawDetectedMarkers", img)
+
+        if cv2.waitKey(10) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+    # 処理時間をCSVファイルに書き込む
+    with open("processing_times.csv", "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(
+            [
+                "Frame Capture (t2-t1)",
+                "Processing (t3-t2)",
+                "Sending (t4-t3)",
+                "Total Time (t4-t1)",
+            ]
+        )
+        csv_writer.writerows(time_measurements)
+
+
+if __name__ == "__main__":
     main()
